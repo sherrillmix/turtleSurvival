403ab127a8a4baed982b22c07f3e179c9e14430a
sherrillmix <sherrillmix@users.noreply.github.com>
2016-01-16 03:29:53 -0500

    Deal with ambiguous release and read in additional files

--- readData.R
+++ readData.R
@@ -24,12 +24,18 @@ calcDeployDay<-function(animals,rdate,deployDates){
 tagData<-readWild('processed/PAT Data-Summary.csv')
 statusData<-readWild('processed/PAT Data-Status.csv')
 statusData$rReleaseTime<-parse_date_time(statusData$ReleaseTime,'%H:%M:%S %d-%b-%y')
-tapply(statusData$ReleaseTime,statusData$Ptt,function(x){
-  tab<-sort(table(x[x!='']))
+releaseDates<-unlist(by(statusData,statusData$Ptt,function(x){
+  tab<-sort(table(x$ReleaseTime[!is.na(x$rReleaseTime)]))
   if(length(tab)==0)return(NA)
-  if(tail(tab,1)/sum(tab)<.7)print(tab)
+  if(tail(tab,1)/sum(tab)<.7)warning(sprintf('Ambiguous release time in PTT %s',x[1,'Ptt']))
   return(tail(names(tab),1))
-})
+}))
+pdt<-readWild('processed/PAT Data-PDTs.csv')
+pdt<-pdt[pdt$BrokenThermistor!=0&is.na(pdt$Partial),]
+
+info$releaseDate<-parse_date_time(sapply(info$PTTID,function(x)releaseDates[as.character(x)]),'%H:%M:%S %d-%b-%y')
+info$releaseDays<-(as.numeric(info$releaseDate)-as.numeric(info$deployDate))/24/60/60
+
 minMaxDepth<-readWild('processed/PAT Data-MinMaxDepth.csv')
 
 if(any(!info$PTTID %in% tagData$DeployID))warning('Missing tag ',paste(info[!info$PTTID %in% tagData$DeployID,'PTTID'],collapse=''))
@@ -43,4 +49,10 @@ minMaxDepth<-minMaxDepth[minMaxDepth$DeployID %in% info$PTTID,]
 
 statusData$deployDay<-calcDeployDay(statusData$Ptt,statusData$rDate,deployDates)
 minMaxDepth$deployDay<-calcDeployDay(minMaxDepth$Ptt,minMaxDepth$rDate,deployDates)
+
+#only ARGOS reprocess with geo
+locations<-readWild('processed/PAT Data-Locations.csv')
+
+histos<-readWild('processed/PAT Data-Histos.csv')
+divePdt<-readWild('processed/PAT Data-DivePDT.csv')
 
