3ff54e4c7c149c1c1a60039986874c2c96196231
sherrillmix <sherrillmix@users.noreply.github.com>
2016-01-16 03:31:16 -0500

    Add release to plot

--- exploreData.R
+++ exploreData.R
@@ -1,11 +1,12 @@
 source('~/scripts/R/dna.R')
 if(!exists('tagData'))source('readData.R')
-statusCols<-c('HauledOut', 'BrokenThermistor', 'BrokenLink', 'NoDawnDusk', 'ReleaseType', 'ReleaseTime', 'InitiallyBroken', 'BurnMinutes', 'FastGPSPower', 'TWICPower', 'PowerLimit', 'WetDry', 'StatusWord', 'Resets', 'PreReleaseTilt', 'PreReleaseTiltSd', 'PreReleaseTiltCount', 'XmitQueue', 'FastGPSLocNumber', 'FastGPSFailures', 'BattDiscon')
+statusCols<-c('HauledOut', 'BrokenThermistor', 'BrokenLink', 'NoDawnDusk', 'ReleaseType', 'InitiallyBroken', 'BurnMinutes', 'FastGPSPower', 'TWICPower', 'PowerLimit', 'WetDry', 'StatusWord', 'Resets', 'PreReleaseTilt', 'PreReleaseTiltSd', 'PreReleaseTiltCount', 'XmitQueue', 'FastGPSLocNumber', 'FastGPSFailures', 'BattDiscon')
 pdf('out/minMaxDepth.pdf')
 for(ii in unique(minMaxDepth$Ptt)){
   thisData<-minMaxDepth[minMaxDepth$DeployID==ii&!is.na(minMaxDepth$MinDepth)&!is.na(minMaxDepth$MaxDepth),]
   thisStatus<-statusData[statusData$Ptt==ii,]
   thisStatus<-thisStatus[apply(thisStatus[,statusCols],1,function(x)any(!is.na(x)&x!=''&x!=0)),]
+  thisInfo<-info[info$PTTID==ii,]
   xlim<-range(thisData$deployDay)
   ylim<-range(c(thisData$MinDepth,thisData$MaxDepth))
   plot(1,1,type='n',main=ii,xlim=xlim+c(-.2,.2)*diff(xlim),ylim=rev(ylim),xlab='Deploy day',ylab='Depth')
@@ -24,6 +25,7 @@ for(ii in unique(minMaxDepth$Ptt)){
     text(apply(selectRanges[,c('startDay','endDay')],1,mean),yPos,paste(status,ifelse(thisStatus[selectRanges$start,status]!=1,thisStatus[selectRanges$start,status],'')),col='red',srt=45)
     abline(v=c(selectRanges$startDay,selectRanges$endDay),lty=3,col='red')
     segments(selectRanges$startDay,yPos,selectRanges$endDay,yPos,lty=3,col='#FF000077')
+    abline(v=thisInfo$releaseDays,col='#0000FF77',lty=2)
   }
 }
 dev.off()
