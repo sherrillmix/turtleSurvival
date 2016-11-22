e6ae0e90d919b7b9addc37d8a2cc85fe4af4514d
sherrillmix <sherrillmix@users.noreply.github.com>
2016-11-22 09:32:17 -0500

    Make hooking location column

--- readOthers.R
+++ readOthers.R
@@ -2,9 +2,13 @@
 sasso<-read.csv('other/sasso.csv',stringsAsFactors=FALSE)
 swimmer<-read.csv('other/swimmer.csv',stringsAsFactors=FALSE)
 
-#note that swimmer text implies that no mortality was observed and that hooking location was used to infer mortality
+#note that swimmer text implies that no mortality was directly observed and that hooking location was used to infer mortality
 swimmer$mortality<-grepl('\\*$',swimmer$hookLoc)
+#note that sasso counts turtles that had reduced diving before release as mortalities
 sasso$mortality<-sasso$fate=='died'
 sasso[is.na(sasso$time),'time']<-0
 swimmer[swimmer$time=='--','time']<-0
 swimmer$time<-as.numeric(swimmer$time)
+#all locations either control or flipper/mouth
+sasso$isDeep<-FALSE
+swimmer$isDeep<-grepl('Deep',swimmer$hookLoc)
